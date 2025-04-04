70fded0c32f09865d75a054a4b269915c6cae251
joeldeondsouza <jdeondsouza@gmail.com>
2025-04-04 21:53:35 +0200

    Initial Commit

---
+++ AudiPilot/AudiPilotApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudiPilotApp.swift
+//  AudiPilot
+//
+//  Created by Dambi on 04.04.25.
+//
+
+import SwiftUI
+
+@main
+struct AudiPilotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AudiPilotTests/AudiPilotTests.swift
@@ -0,0 +1,17 @@
+//
+//  AudiPilotTests.swift
+//  AudiPilotTests
+//
+//  Created by Dambi on 04.04.25.
+//
+
+import Testing
+@testable import AudiPilot
+
+struct AudiPilotTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
